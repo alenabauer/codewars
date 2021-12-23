e2bd6a23477a8d6e5dce9b8104493947e76c6ba8
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-23 11:44:35 +0100

    Improve time efficiency of the Ruby solution for kata: Sum of pairs

--- 5_kyu/Sum of pairs/solution.rb
+++ 5_kyu/Sum of pairs/solution.rb
@@ -1,5 +1,8 @@
 def sum_pairs(ints, s)
-  combinations = ints.each_cons(2).to_a
-  other_combinations = ints.combination(2).to_a - combinations
-  combinations.find { |x| x.sum == s } || other_combinations.find { |x| x.sum == s }
+  seen = Hash.new
+  for int in ints
+    return [s - int, int] if seen.keys.include?(s - int)
+    seen[int] = true
+  end
+  nil
 end
