a0747713c495f3546b97f0d885608f42e977f2c9
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-23 11:47:19 +0100

    Refactor

--- 5_kyu/Sum of pairs/solution.rb
+++ 5_kyu/Sum of pairs/solution.rb
@@ -1,7 +1,7 @@
 def sum_pairs(ints, s)
   seen = Hash.new
   for int in ints
-    return [s - int, int] if seen.keys.include?(s - int)
+    return [s - int, int] if seen[s - int]
     seen[int] = true
   end
   nil
