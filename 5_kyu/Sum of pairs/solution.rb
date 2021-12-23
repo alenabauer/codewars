e00b0139f62b77cd45050c5c2a565df588711049
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-23 11:14:48 +0100

    Refactor Ruby solution for kata: Sum of pairs

--- 5_kyu/Sum of pairs/solution.rb
+++ 5_kyu/Sum of pairs/solution.rb
@@ -1,16 +1,5 @@
 def sum_pairs(ints, s)
-  cons = ints.each_cons(2).to_a
-  i = 0
-  while i < cons.length
-    return cons[i] if cons[i][0] + cons[i][1] == s
-    i += 1
-  end
-  combinations = ints.combination(2).to_a
-  i = 0
-  while i < combinations.length
-    return combinations[i] if combinations[i][0] + combinations[i][1] == s
-    i += 1
-  end
+  combinations = ints.each_cons(2).to_a
+  other_combinations = ints.combination(2).to_a - combinations
+  combinations.find { |x| x.sum == s } || other_combinations.find { |x| x.sum == s }
 end
-
-print sum_pairs([10, 5, 2, 3, 7, 5], 10)
