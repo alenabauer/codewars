9fed96ba05732192773f3fe7839583f9d52fec73
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-27 17:30:53 +0100

    Add comments

--- 4_kyu/Strings Mix/solution.rb
+++ 4_kyu/Strings Mix/solution.rb
@@ -1,8 +1,11 @@
 def mix(s1, s2)
+  # filter strings to select only downcase letters
   s1chars = s1.chars.select { |char| ('a'..'z').include?(char) }
   s2chars = s2.chars.select { |char| ('a'..'z').include?(char) }
+  # group letters by their frequency in each string
   s1freqs = s1chars.group_by(&:itself).transform_values!(&:size).select { |k, v| v > 1 }
   s2freqs = s2chars.group_by(&:itself).transform_values!(&:size).select { |k, v| v > 1 }
+  # merge letters and their frequency into one hash
   merged = {}
   s1freqs.each do |k, v|
     merged[k] = v
@@ -11,6 +14,7 @@ def mix(s1, s2)
     merged[k] = v unless merged[k] && merged[k] > v
   end
   merged = merged.sort_by { |x, y| [ -Integer(y), x ] }.to_h
+  # resort the hash
   hash = {}
   merged.each do |k, v|
     new_value = ""
@@ -25,14 +29,10 @@ def mix(s1, s2)
     hash[new_key] = new_value
   end
   resorted = hash.sort_by { |k, v| [-k.chars.size, v] }.to_a.map { |sub| sub[1] == 61 ? [sub[0], sub[1].chr] : sub }
+  # build the output string
   output = ""
   resorted.each do |pair|
     output += "#{pair[1]}:#{pair[0]}/"
   end
-  return output.chop
+  output.chop
 end
-
-s1="Are the kids at home? aaaaa fffff"
-s2="Yes they are here! aaaaa fffff"
-
-print mix(s1, s2)
