731be49e2599fb430a1ac52ed2db6aa26130c0dd
Alena Bauer <alena.a.bauer@gmail.com>
2022-01-03 16:22:27 +0100

    Refactor

--- 6_kyu/Dashatize it/solution.js
+++ 6_kyu/Dashatize it/solution.js
@@ -3,8 +3,8 @@ function dashatize(num) {
     if (!num) return String(num);
     num = Math.abs(num);
     let str = [...String(num)].map(x => Number(x) % 2 != 0 ? `-${x}-` : x).join('');
-    if (str.charAt(0) == '-') str = str.substring(1);
-    if (str.charAt(str.length - 1) == '-') str = str.substring(0, str.length - 1);
+    if (str.at(0) == '-') str = str.substring(1);
+    if (str.at(-1) == '-') str = str.substring(0, str.length - 1);
     return str.replace(/--/g, '-');
   } else {
     return "";
